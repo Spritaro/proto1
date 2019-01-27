239c746922c7dc93306616524123ce4153890a17
Spritaro <k5dot.feb@gmail.com>
2019-01-27 11:34:19 +0900

    delete servo message

--- proto1_ros/scripts/head.py
+++ proto1_ros/scripts/head.py
@@ -4,8 +4,46 @@
 import rospy
 import tf.transformations
 from geometry_msgs.msg import PoseStamped
-from proto1_ros.msg import proto1_servo, proto1_servo_multi
 from math import pi
+import Adafruit_PCA9685
+
+
+SERVOMIN = 103
+SERVOMAX = 490
+SERVONUM = 2
+
+class Servo(object):
+    id = 0
+    end_angle = 0.0         # in degree
+    current_angle = 0.0     # in degree
+    start_angle = 90.0       # in degree
+    end_time = 1            # in 10ms
+    current_time = 0        # in 10ms
+    start_time = 0          # always 0
+    power = False
+
+servos = [ Servo() for i in range(SERVONUM) ]
+
+
+def conv_ang(ang):
+    return int((SERVOMAX - SERVOMIN) * (ang + 90.0) / 180.0 + SERVOMIN)
+
+
+def servo_update():
+    global servos
+    for i, servo in enumerate(servos):
+        if(servo.power):
+            if(servo.current_time < servo.end_time):
+                servo.current_angle = (servo.end_angle - servo.start_angle) * (servo.current_time - servo.start_time) / (servo.end_time - servo.start_time) + servo.start_angle
+                servo.current_time += 1 # 10ms
+            if(servo.current_time >= servo.end_time):
+                servo.current_angle = servo.end_angle
+            # set updated angle
+            pwm.set_pwm(servo.id, 0, conv_ang(servo.current_angle))
+        else:
+            # power off
+            pwm.set_pwm(servo.id, 0, 0)
+
 
 def head_callback(msg):
     # rospy.loginfo("received head orientation")
@@ -33,31 +71,28 @@ def head_callback(msg):
 
     rospy.loginfo("pitch{}, yaw{}".format(head_pitch, head_yaw))
 
-    # create message
-    servo_p = proto1_servo()
-    servo_p.id = 0
-    servo_p.time = 0
-    servo_p.power = True
-    servo_p.angle = head_pitch
+    servos[0].id = 0
+    servos[0].end_time = 0
+    servos[0].end_angle = head_pitch
+    servos[0].power = True
 
-    servo_y = proto1_servo()
-    servo_y.id = 15
-    servo_y.time = 0
-    servo_y.power = True
-    servo_y.angle = head_yaw
+    servos[1].id = 15
+    servos[1].end_time = 0
+    servos[1].end_angle = head_yaw
+    servos[1].power = True
 
-    servo_multi = proto1_servo_multi()
-    servo_multi.count = 2               # number of servo motors
-    servo_multi.servo.append(servo_p)   # pitch servo data
-    servo_multi.servo.append(servo_y)   # yaw servo data
+    servo_update()
 
-    # publish message
-    pub.publish(servo_multi)
 
 if __name__ == '__main__':
     # setup ros node
-    pub = rospy.Publisher('ServoAngles', proto1_servo_multi, queue_size=0)
     rospy.loginfo("setting up ros node")
     rospy.init_node('head')
     sub = rospy.Subscriber('/camera/pose_stamped', PoseStamped, head_callback)
+
+    # setup servo motors
+    rospy.loginfo("setting servo motors")
+    pwm = Adafruit_PCA9685.PCA9685()
+    pwm.set_pwm_freq(50)    # 50Hz -> 20ms
+
     rospy.spin()
